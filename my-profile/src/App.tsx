2f0d1d837010452384b692a9d249224d4321ab52
theskyworld <2456450687@qq.com>
2023-10-11 20:43:18 +0800

    feat(components) : add Projects

--- my-profile/src/App.tsx
+++ my-profile/src/App.tsx
@@ -1,6 +1,7 @@
 import About from './components/About/About'
 import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar'
+import Projects from './components/Projects/Projects'
 import Services from './components/Services/Services'
 import './styles/index.css'
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Projects />
     </div>
   )
 }
---
+++ my-profile/src/components/Projects/Projects.tsx
@@ -0,0 +1,103 @@
+import "//at.alicdn.com/t/c/font_4282296_wnjdqgjjox.js";
+import portfolio1Jpg from "@/assets/imgs/portfolio1.jpg";
+
+
+export default function Projects() {
+    return (
+        <section className="portfolio" id="portfolio">
+            <h2 className="heading">Latest <span>Project</span></h2>
+
+            <div className="portfolio-container">
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src={portfolio1Jpg} alt="" />
+
+                    <div className="portfolio-layer">
+                        <h4>Web Design</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam est.</p>
+                        <a href="#">
+                            <i>
+                                <svg className="icon" aria-hidden="true">
+                                    <use xlinkHref="#icon-fenxiang"></use>
+                                </svg>
+                            </i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
