6e976d60adb98ed235688808965763b5f411c592
theskyworld <2456450687@qq.com>
2024-02-23 17:23:37 +0800

    refactor: revise Footer

--- my-profile/src/components/Footer/Footer.tsx
+++ my-profile/src/components/Footer/Footer.tsx
@@ -4,16 +4,12 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-text">
-                <p>Copyright &copy; 2023 by Codehal | All Rights Reserved.</p>
+                <p>Copyright &copy; 2024 by TSW | All Rights Reserved.</p>
             </div>
 
             <div className="footer-iconTop">
                 <a href="home">
-                    <i>
-                        <svg style={{ backgroundColor: "red", width: "1rem", height : "1rem" }} className="icon" aria-hidden="true">
-                            <use xlinkHref="#icon-up"></use>
-                        </svg>
-                    </i>
+                    ⬆
                 </a>
             </div>
         </footer>
